dd1b62776e676885c8e2f062e75afb5890977676
computatorium <33630760+computatorium@users.noreply.github.com>
2026-04-05 22:48:36 -0500

    rbi: fix including Box

--- ruby/rbi/rbi.rb
+++ ruby/rbi/rbi.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # 05-04-26 - rbi - ruby info, prints all constants under Ruby::
-Ruby.constants.each { |c| puts "#{c.to_s} - #{Ruby.const_get(c)}" }
+Ruby.constants.each { |c| puts "#{c.to_s} - #{Ruby.const_get(c)}" unless c == :Box }
