2afd86965a75262905a5ebae5fc270ef71464364
computatorium <33630760+computatorium@users.noreply.github.com>
2026-04-05 23:11:10 -0500

    freeze literals in rbi and make it portable and usable as a library (#rbi method)

--- ruby/rbi/rbi.rb
+++ ruby/rbi/rbi.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
-# 05-04-26 - rbi - ruby info, prints all constants under Ruby::
-Ruby.constants.each { |c| puts "#{c.to_s} - #{Ruby.const_get(c)}" unless c == :Box }
+# frozen_string_literal: true
+
+# 05-04-26 - rbi - ruby info, all constants under Ruby::
+def rbi
+  Ruby.constants.reject { |c| c == :Box }.to_h { |c| [c, Ruby.const_get(c)] }
+end
+
+rbi.each_pair { |k, v| puts "#{k}: #{v}" } if __FILE__ == $PROGRAM_NAME
