595524f76f61e0d30525d9631d62f3bf8ef04c98
computatorium <33630760+computatorium@users.noreply.github.com>
2026-09-23 14:05:14 -0500

    updated kv & rbi

--- ruby/rbi/rbi.rb
+++ ruby/rbi/rbi.rb
@@ -2,8 +2,17 @@
 # frozen_string_literal: true
 
 # 05-04-26 - rbi - ruby info, all constants under Ruby::
-def rbi
-  Ruby.constants.reject { |c| c == :Box }.to_h { |c| [c, Ruby.const_get(c)] }
+# 09-22-26 - updated - added a more robust algorithm for getting the constants, and predicates for RUBY_ENGINE
+module Rbi
+  Ruby.constants.reject { |c| c == :Box }.to_h { |c| [c, Ruby.const_get(c)] }.each_pair { |k, v| const_set(k, v) }
+
+  def mri? = RUBY_ENGINE == "ruby"
+
+  def jruby? = RUBY_ENGINE == "jruby"
+
+  def truffleruby? = RUBY_ENGINE == "truffleruby"
+
+  def rubinius? = RUBY_ENGINE == "rbx"
 end
 
-rbi.each_pair { |k, v| puts "#{k}: #{v}" } if __FILE__ == $PROGRAM_NAME
+Rbi.constants.each { |c| puts "#{c}: #{Rbi.const_get(c)}" } if __FILE__ == $PROGRAM_NAME
